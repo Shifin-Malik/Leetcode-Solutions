203784268cf3bcca63427a3e4688b96a9b355a3b
Shifin Malik <137679921+Shifin-Malik@users.noreply.github.com>
2025-12-24 10:43:28 +0530

    Time: 4 ms (50.16%), Space: 60.2 MB (26.79%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ 
+**/
+const findMedianSortedArrays = (nums1, nums2) => {
+   let arr = nums1.concat(nums2).sort((a,b) => a - b);
+   let len = arr.length;
+   
+   if(len % 2 === 1) {
+    return arr[Math.floor(len / 2)]      
+   } else {
+    return (arr[len / 2 - 1] + arr[len / 2]) / 2
+   }
+   
+}
