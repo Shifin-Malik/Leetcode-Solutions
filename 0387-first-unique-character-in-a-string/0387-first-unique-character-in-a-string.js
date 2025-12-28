5fd0ec0f17c260f0afbe3141ae2c15b11b06e444
Shifin Malik <137679921+Shifin-Malik@users.noreply.github.com>
2025-12-28 21:22:19 +0530

    Time: 691 ms (5.3%), Space: 86 MB (5.02%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    for(let i in s){
+        if(s.indexOf(s[i]) === s.lastIndexOf(s[i])){
+            return Number(i)
+        }
+    }
+    return -1
+};
