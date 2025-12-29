b332b02c8c5fbe5c66029857fa8425177d6ca260
Shifin Malik <137679921+Shifin-Malik@users.noreply.github.com>
2025-12-29 20:25:01 +0530

    Time: 0 ms (100%), Space: 57.1 MB (82.45%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -1,15 +1,14 @@
 function fizzBuzz(n){
-   let arr = [];
-   for(let i = 1; i <= n; i++){
-     if(i % 3 === 0 && i % 5 === 0){
-         arr.push('FizzBuzz')
-     } else if (i % 3 === 0){
-         arr.push('Fizz')
-     } else if (i % 5 === 0){
-         arr.push('Buzz')
-     } else {
-         arr.push(i.toString())
-     }
-   }
-   return arr
+  let arr = [];
+
+  for(let i = 1; i <= n; i++){
+    let str = '';
+
+    if(i % 3 === 0) str += 'Fizz';
+    if(i % 5 === 0) str += 'Buzz';
+
+    arr.push(str || i.toString());
+  }
+
+  return arr;
 }
