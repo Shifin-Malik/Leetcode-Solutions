65988812d03f0edc8e23f2171e5fe528198b2bbc
Shifin Malik <137679921+Shifin-Malik@users.noreply.github.com>
2026-01-01 23:48:13 +0530

    Time: 4 ms (75.49%), Space: 57.6 MB (68.7%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -5,8 +5,5 @@
 var isPalindrome = function(s) {
   let str = s.toLowerCase().replace(/[^a-z0-9]/g,"")
   let rev = str.split('').reverse().join('');
-  if(str === rev) {
-      return true;
-  }  
-   return false;
+  return str === rev 
 };
