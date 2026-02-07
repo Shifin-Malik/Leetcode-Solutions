5d9554bde9499719a52429229c7b98adf4ad58c7
Shifin Malik <137679921+Shifin-Malik@users.noreply.github.com>
2026-02-07 17:51:25 +0530

    Time: 14 ms (85.78%), Space: 73.9 MB (60.16%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -1,10 +1,3 @@
 var containsDuplicate = function(nums) {
-    let sort = nums.sort((a, b) => a - b);
-    let out = []
-    for(let i = 0; i < sort.length;i++){
-        if(sort[i] === sort[i + 1]){
-            return true
-        }
-    }
-    return false
+   return nums.length !== new Set(nums).size
 };
