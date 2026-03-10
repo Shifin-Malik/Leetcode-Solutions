d6bfac35c7a98b58705779ba15a641f118a24040
Shifin Malik <137679921+Shifin-Malik@users.noreply.github.com>
2026-03-10 11:58:16 +0530

    Time: 4 ms (98.67%), Space: 61.4 MB (14.32%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -4,11 +4,11 @@
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    for (let char of ransomNote) {
-        if (!magazine.includes(char)) {
-            return false;
-        }
-        magazine = magazine.replace(char, "");
-    }
-    return true;
+   for(let char of ransomNote){
+    if(!magazine.includes(char)){
+        return false
+    };
+    magazine = magazine.replace(char, "");
+   }
+   return true
 };
