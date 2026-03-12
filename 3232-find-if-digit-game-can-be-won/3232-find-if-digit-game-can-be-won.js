a63cf379e2d1de62c61d494402ff43cabff73ca7
Shifin Malik <137679921+Shifin-Malik@users.noreply.github.com>
2026-03-12 10:28:24 +0530

    Time: 0 ms (100%), Space: 57.1 MB (28.74%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.js
@@ -0,0 +1,17 @@
+var canAliceWin = function(nums) {
+    let single = 0;
+    let double = 0;
+    for(let num of nums){
+       if(num < 10){
+        single += num
+       } else {
+        double += num
+       };
+    };
+    
+    if(single !== double){
+        return true
+    } else {
+        return false
+    }
+};
