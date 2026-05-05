763a2defd8a42afbf7c9fb068ca1e8d5f0c77eeb
Shifin Malik <137679921+Shifin-Malik@users.noreply.github.com>
2026-05-05 10:50:50 +0530

    Time: 5 ms (23.03%), Space: 56.3 MB (59.54%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.js
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.js
@@ -4,16 +4,18 @@
  */
 var findLucky = function(arr) {
     let counts = {};
+
     for(let num of arr){
         counts[num] = (counts[num] || 0) + 1
     };
-    
-    let result = -1
-    
+
+    let result = -1;
+
     for(let num in counts){
         if(Number(num) === counts[num]){
             result = Math.max(result, Number(num))
         }
     }
+
     return result
 };
